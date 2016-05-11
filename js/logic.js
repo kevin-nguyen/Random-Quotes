6cc14ce191c3c15a8ab7d819292eadc97265153d
Kevin Nguyen <kevin.nguyen@engineering.ucla.edu>
2016-05-11 14:38:52 -0700

    Add color transition to background, icons, and texts

--- js/logic.js
+++ js/logic.js
@@ -2,6 +2,55 @@ $(document).ready(function() {
     var quote, author, index;
     var quoteStack = [];
 
+    function randomColor(mode) {
+        var color = ['#75C9C8', '#C0B9DD', '#0D3B66', '#F4D35E', 
+                     '#EE964B', '#646881', '#232621', '#5AD2F4', 
+                     '#69B578', '#CE6D8B', '#A44A3F', '#B93848'];
+
+        var hexValues = ['F', 'E', 'D', 'C', 'B', 'A', '9', '8','7', '6', '5', '4', '3', '2', '1', '0'];
+        var choice = null;
+
+        if (mode === 'fullRandom') {
+            choice = '#';
+
+            for (var i = 0; i < 6; i++) {
+                choice += hexValues[Math.floor(Math.random() * 16)];
+            }
+        } else {
+            choice = color[Math.floor(Math.random() * color.length)];
+        }
+
+        console.log(choice);
+        return choice;
+    }
+
+    function changeColor() {
+        var backgroundColor = randomColor();
+        $('body').css('background-color', backgroundColor);
+        $('.fa-twitter-square, #refresh').css('color', backgroundColor);
+        $('#quote').css('color', randomColor('fullRandom'));
+        $('#author').css('color', randomColor('fullRandom'));
+
+        if (! $('#prev').hasClass('disabled')) $('#prev').css('color', backgroundColor);
+        if (! $('#next').hasClass('disabled')) $('#next').css('color', backgroundColor);
+
+    }
+
+    function drawText() {
+        $('#quote, #author').animate({opacity: 0}, 750, function() {
+            $('#quote, #author').animate({opacity: 1}, 750);
+            $("#quote").text(quote);
+            $("#author").text(author).prepend("&mdash; ");
+        });
+    }
+
+    function createTweetLink() {
+        var encodeQuote ='"' + encodeURIComponent(quote) + '"';
+        var encodeAuthor = " " + encodeURIComponent(author);
+        var uri = "https://twitter.com/intent/tweet?hashtags=QuoteOfTheDay&text=" + encodeQuote + encodeAuthor;
+        $(".tweet").attr('href', uri);
+    }
+
     function requestQuote() {
         $.ajax({
             url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous",
@@ -21,13 +70,9 @@ $(document).ready(function() {
             quote = response.quote;
             author = response.author;
 
-            $("#quote").text(quote);
-            $("#author").text(author).prepend("&mdash; ");
-
-            var encodeQuote ='"' + encodeURIComponent(quote) + '"';
-            var encodeAuthor = " " + encodeURIComponent(author);
-            var uri = "https://twitter.com/intent/tweet?hashtags=QuoteOfTheDay&text=" + encodeQuote + encodeAuthor;
-            $(".tweet").attr('href', uri);
+            drawText();            
+            changeColor();
+            createTweetLink();
         })
         .fail(function(jqXHR, textStatus, errorThrown) {
 
@@ -43,46 +88,38 @@ $(document).ready(function() {
     });
 
     $('#prev').on('click', function(event) {
-        if (!$('#prev').hasClass('disabled')) {
+        if (! $('#prev').hasClass('disabled')) {
             index -= 1;
 
             if (index >= 0) {
                 quote = quoteStack[index].quote;
                 author = quoteStack[index].author;
 
-                $("#quote").text(quote);
-                $("#author").text(author).prepend("&mdash; ");
-
-                var encodeQuote ='"' + encodeURIComponent(quote) + '"';
-                var encodeAuthor = " " + encodeURIComponent(author);
-                var uri = "https://twitter.com/intent/tweet?hashtags=QuoteOfTheDay&text=" + encodeQuote + encodeAuthor;
-                $(".tweet").attr('href', uri);
+                drawText();                        
+                createTweetLink();
             }
 
             $('#prev').toggleClass('disabled', (index) === 0);
             $('#next').toggleClass('disabled', (index) === quoteStack.length - 1);
+            changeColor();
         }   
     });
 
     $('#next').on('click', function(event) {
-        if (!$('#next').hasClass('disabled')) {
+        if (! $('#next').hasClass('disabled')) {
             index += 1;
 
             if (index >= 0 && index < quoteStack.length) {
                 quote = quoteStack[index].quote;
                 author = quoteStack[index].author;
                 
-                $("#quote").text(quote);
-                $("#author").text(author).prepend("&mdash; ");
-
-                var encodeQuote ='"' + encodeURIComponent(quote) + '"';
-                var encodeAuthor = " " + encodeURIComponent(author);
-                var uri = "https://twitter.com/intent/tweet?hashtags=QuoteOfTheDay&text=" + encodeQuote + encodeAuthor;
-                $(".tweet").attr('href', uri);
+                drawText();            
+                createTweetLink();
             } 
 
             $('#prev').toggleClass('disabled', (index) === 0);
             $('#next').toggleClass('disabled', (index) === quoteStack.length - 1);
+            changeColor();
         }        
     });
 });
